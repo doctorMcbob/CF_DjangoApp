ccba31926a108e8dc463123f1fc90bb64373d2ac
Wesley <wootenwesley@gmail.com>
2015-03-17 19:36:06 -0700

    fix

--- prob3/usradmin/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from usradmin.models import BasicUser
-
-# Register your models here.
-admin.site.register(BasicUser)
--- prob3/usradmin/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class BasicUser(models.Model):
-    first = models.CharField(max_length=20)
-    last = models.CharField(max_length=20, blank=True)
-    email = models.EmailField(max_length=254)
